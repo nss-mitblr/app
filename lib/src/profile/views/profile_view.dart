ec1c60767ba887fa16e63c0dda0be1ffc68fd7f5
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-08-15 15:34:10 +0530

    fix(views): convert profile to normal scrollable view

--- lib/src/profile/views/profile_view.dart
+++ lib/src/profile/views/profile_view.dart
@@ -5,82 +5,105 @@ import 'package:nss/src/profile/components/profile_menu_widget.dart';
 class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(18.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 12.0,
-                ),
-
-                /// -- IMAGE
-                SizedBox(
-                  width: 120,
-                  height: 120,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: const Image(
-                      image: AssetImage('assets/logo.png'),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text('Abhigyan Tripathi',
-                    style:
-                        TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 8),
-                Text('CSE Cybersecurity (B), 2026',
-                    style: Theme.of(context).textTheme.bodyMedium),
-                const SizedBox(height: 20),
-
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        side: BorderSide.none, shape: const StadiumBorder()),
-                    child: const Text('Edit Profile'),
-                  ),
-                ),
-                const SizedBox(height: 20),
-
-                /// -- MENU
-                ProfileMenuWidget(
-                  title: 'Settings',
-                  icon: Icons.settings,
-                  onPressed: () {},
-                ),
-                ProfileMenuWidget(
-                  title: 'Academic Details',
-                  icon: Icons.school,
-                  onPressed: () {},
-                ),
-                ProfileMenuWidget(
-                  title: 'User Management',
-                  icon: Icons.manage_accounts,
-                  onPressed: () {},
-                ),
-                const SizedBox(height: 10),
-                const Divider(),
-                const SizedBox(height: 10),
-                ProfileMenuWidget(
-                  title: 'Information',
-                  icon: Icons.info,
-                  onPressed: () {},
-                ),
-                ProfileMenuWidget(
-                  title: 'Logout',
-                  icon: Icons.logout,
-                  textColor: Colors.red,
-                  endIcon: false,
-                  onPressed: () {},
-                ),
-              ],
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 84.0,
             ),
-          ),
+
+            /// -- IMAGE
+            SizedBox(
+              width: 120,
+              height: 120,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: const Image(
+                  image: AssetImage('assets/logo.png'),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text('Abhigyan Tripathi',
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 8),
+            Text('CSE Cybersecurity (B), 2026',
+                style: Theme.of(context).textTheme.bodyMedium),
+            const SizedBox(height: 20),
+
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    side: BorderSide.none, shape: const StadiumBorder()),
+                child: const Text('Edit Profile'),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            /// -- MENU
+            ProfileMenuWidget(
+              title: 'Settings',
+              icon: Icons.settings,
+              onPressed: () {},
+            ),
+            ProfileMenuWidget(
+              title: 'Academic Details',
+              icon: Icons.school,
+              onPressed: () {},
+            ),
+            ProfileMenuWidget(
+              title: 'User Management',
+              icon: Icons.manage_accounts,
+              onPressed: () {},
+            ),
+            const SizedBox(height: 10),
+            const Divider(),
+            const SizedBox(height: 10),
+            ProfileMenuWidget(
+              title: 'Information',
+              icon: Icons.info,
+              onPressed: () {},
+            ),
+            ProfileMenuWidget(
+              title: 'Logout',
+              icon: Icons.logout,
+              textColor: Colors.red,
+              endIcon: false,
+              onPressed: () {},
+            ),
+            ProfileMenuWidget(
+              title: 'Information',
+              icon: Icons.info,
+              onPressed: () {},
+            ),
+            ProfileMenuWidget(
+              title: 'Logout',
+              icon: Icons.logout,
+              textColor: Colors.red,
+              endIcon: false,
+              onPressed: () {},
+            ),
+            ProfileMenuWidget(
+              title: 'Information',
+              icon: Icons.info,
+              onPressed: () {},
+            ),
+            ProfileMenuWidget(
+              title: 'Logout',
+              icon: Icons.logout,
+              textColor: Colors.red,
+              endIcon: false,
+              onPressed: () {},
+            ),
+
+            const SizedBox(
+              height: 96,
+            )
+          ],
         ),
       ),
     );
