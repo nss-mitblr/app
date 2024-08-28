ac2b2e783f248f940ca714e0b8e315b1ee83b8ae
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-08-28 22:07:27 +0530

    fix(db): init hive on open

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'package:nss/src/app.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('nss');
+
   runApp(const ProviderScope(child: MainApp()));
 }
